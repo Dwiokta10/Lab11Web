14b1782a05e75df6145ffddcffbf04f000a2f872
Dwiokta10 <dwi.okta.ramadhani.005@gmail.com>
2025-12-16 16:05:48 +0700

    Delete lab11_php_oop directory

--- lab11_php_oop/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = [
-    'host' => '127.0.0.1:3307',
-    'username' => 'root',
-    'password' => '', // Sesuaikan dengan password database kamu
-    'db_name' => 'lab11_php_oop'   // <— ganti ke nama DB yang kamu pakai
-];
-?>
--- lab11_php_oop/module/auth/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$baseUrl = defined('APP_BASE_URL') ? APP_BASE_URL : '';
-
-unset($_SESSION['auth_user']);
-
-header('Location: ' . $baseUrl . '/auth/login?logged_out=1');
-exit;
